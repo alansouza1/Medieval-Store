afa45d0817f7f132fe4134e673499dd16b43ca44
alansouza1 <alansouza4001@gmail.com>
2026-03-31 13:56:19 -0300

    feat: configure order routes and update navigation in app component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
+import { OrderListComponent } from './components/order-list/order-list';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'products', component: ProductListComponent },
+  { path: 'orders', component: OrderListComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
