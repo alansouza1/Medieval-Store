ff1ce94ed2c5c0bcfc705c4d4fa553cef79c8c6f
alansouza1 <alansouza4001@gmail.com>
2026-03-31 13:55:32 -0300

    feat: implement order service in frontend

--- frontend/src/app/services/order.ts
+++ frontend/src/app/services/order.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Order {
+  id: number;
+  userId: number;
+  productsIds: number[];
+}
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Order {}
+export class OrderService {
+  private apiUrl = 'http://localhost:3000/orders';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiUrl);
+  }
+}
