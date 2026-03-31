963f6fd0eecef13a6db0a96ffc001eadeb632500
alansouza1 <alansouza4001@gmail.com>
2026-03-31 13:44:08 -0300

    feat: configure angular routing and update app component for authentication flow

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list';
+import { LoginComponent } from './components/login/login';
+import { RegisterComponent } from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
